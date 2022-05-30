bab6418cbba69296af95c27890762f247fe8653b
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-05-30 14:15:01 +0900

    Create 자료형.py

---
+++ 자료형.py
@@ -0,0 +1,24 @@
+#정수형 자료형
+print(5) # 5
+print(-10) # -10
+print(3.14) # 3.14
+print(1000) # 1000
+print(5+3) # 8
+print(2*8) # 16
+print(3*(3+1)) # 12
+
+#문자열 자료형
+print('풍선') # 풍선
+print('나비') # 나비
+print('ㅋㅋ') # ㅋㅋ
+print('ㅋ' * 10) # ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+
+#boolean 자료형
+print(5 > 10) # False
+print(5 < 10) # True
+print(True) # True
+print(False) # False
+#not을 붙이면 반대로 출력이됨
+print(not True) # False
+print(not False) # True
+print(not(5>10)) # true
