a53ab8c101d419dc39d5c10b88b944d25476033f
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-05-30 14:47:51 +0900

    Create 숫자처리함수.py

---
+++ 숫자처리함수.py
@@ -0,0 +1,26 @@
+#절대값 함수 abs()
+print(abs(-5)) # 5
+
+#제곱 함수 pow()
+print(pow(4,2)) # 4^2 = 16
+
+#최대값을 나타내는 함수  max()
+print(max(5,12)) # 12
+
+#최소값을 나타내는 함수 min()
+print(min(5,12)) # 5
+
+#소수점을 반올림 하는 함수 round()
+print(round(3.14)) # 3
+print(round(4.99)) # 5
+
+#math 라이브러리를 임포트 하면 더 많은 수학 함수를 사용할 수 있음
+from math import * #이렇게 라이브러리를 임포트 해야함
+#실수 내림 함수 floor()
+print(floor(4.99)) # 4
+
+#실수 올림 함수 ceil()
+print(ceil(3.14))
+
+#제곱근 구하는 함수 sqrt()
+print(sqrt(16)) #4
