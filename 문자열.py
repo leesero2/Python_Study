891a1664b6a8a3d708d27a4949f9c79c585472bd
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-06-01 17:05:34 +0900

    Create 문자열.py

---
+++ 문자열.py
@@ -0,0 +1,16 @@
+sentence = '나는 소년입니다'
+print(sentence) # 나는 소년입니다
+
+sentence2 = "파이썬은 쉬워요"
+print(sentence2) # 파이썬은 쉬워요
+
+sentence3 = """
+나는 소년이고,
+파이썬은 쉬워요
+"""
+print(sentence3)
+#
+#나는 소년이고,
+#파이썬은 쉬워요
+#
+# """ 큰따옴표 세개는 줄바꿈기능 까지 전부 포함시킴
