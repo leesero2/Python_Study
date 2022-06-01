508865fa0cd9cfb7aa51c6434fa0ea4d6d380a71
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-06-01 18:37:31 +0900

    Create 탈출문자.py

---
+++ 탈출문자.py
@@ -0,0 +1,15 @@
+# \n : 줄바꿈
+print("백문이 불여일견 \n백견이 불여일타")
+
+# 저는 "나도 코딩" 입니다
+print('저는 "나도 코딩" 입니다')
+print("저는 \"나도 코딩\" 입니다")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine") # Pine
+
+# \b : 백스페이스 ( 한글자 삭제 )
+print("Redd\bApple")# RedApple
+
+# \t : 탭
+print("Red\tApple") # Red	Apple (키보드에서 탭을 치는 효과를 냄)
